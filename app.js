1a3335d157e9540f79b05298e6faf94ac265e29a
avijit1998 <avijitnanda14@gmail.com>
2020-08-25 11:00:59 +0530

    Integrated geocode and forecast into one and added commmand line input

--- app.js
+++ app.js
@@ -2,15 +2,22 @@ const request = require("request");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Philadelphia New York", (error, data) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
-});
+const location = process.argv.slice(2).join(" ");
 
-forecast(-75.7088, 44.1545, (error, data) => {
-  if (error) console.log("Error", error);
-  console.log("Forecast:", data);
-});
+if (location) {
+  geocode(location, (error, data) => {
+    if (error) {
+      return console.log(error);
+    } else {
+      forecast(data.longitude, data.latitude, (error, forecastData) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(data.location);
+        console.log("Forecast:", forecastData);
+      });
+    }
+  });
+} else {
+  console.log("Location field empty.");
+}
